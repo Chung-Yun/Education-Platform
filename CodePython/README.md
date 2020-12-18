bdca515526cfae94b365ad2ee01d5d81d76e75d8
Colleabois <juitinglu@users.noreply.github.com>
2020-12-18 07:48:34 +0100

    add webcrawling code

--- CodePython/README.md
+++ CodePython/README.md
@@ -1,1 +1,10 @@
- 這個資料夾是上傳網路爬蟲的地方
+# Webcrawling Hahow's website
+
+This directory stores the codes that webcrawls the information we need in this project.
+
+**Webcrawling_SinglePage.py** is our first attempt to fetch information in from one webpage that is defined in the first place. In **Webcrawling_MultiPage.py** we try to crawl over every courses that has the key word "Python" in the course title. Lastly, **Webcrawling_All.py** manage to find all the comments of the courses that has the key word "Python" in their title. 
+
+
+## Issues regarding implementation
+
+These Python scripts involved using the package, Pandas. We can't install Pandas using Python 3.9 on a MacOS Big Sur. This is why we are using older Python versions.
--- CodePython/WebCrawling_All.py
+++ CodePython/WebCrawling_All.py
@@ -18,135 +18,181 @@ chromedriver_path = os.environ.get("CHROMEDRIVER_PATH")
 if chromedriver_path ==None:
     chromedriver_path = 'chromedriver'
 
-# initialise dataframe
-df = pd.DataFrame({\
-"class_title"   : [], \
-"class_url"     : [], \
-"class_figure"  : [], \
-"stars"         : [], \
-"teacher_name"  : [], \
-"price"         : [], \
-"comment_date"  : [], \
-"comment_title" : [], \
-"comment_text"  : []}) 
 
-target = 'python'
-first_url = 'https://hahow.in/courses?search='+target
-
-# Define total page
-def findTotalPage(myurl):
-    first_driver = webdriver.Chrome(chromedriver_path, options=chrome_options)
-    first_page = first_driver.get(myurl)
+def findTotalPage(my_url):
+    """ Calculates the total page of the search """
+    driver = webdriver.Chrome(chromedriver_path, options=chrome_options)
+    page = driver.get(my_url)
     time.sleep(3) 
-    first_soup = BeautifulSoup(first_driver.page_source, 'html.parser')
-    page_block = first_soup.find_all('ul',{'class':"rc-pagination gbga9a-0 jYLVph"})
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
+    page_block = soup.find_all('ul',{'class':"rc-pagination gbga9a-0 jYLVph"})
     assert len(page_block)==1
     pages_plus_arrows = page_block[0].find_all('li') # 這邊會多算兩個箭頭
-    first_driver.quit()
+    driver.quit()
     return len(pages_plus_arrows) - 2
-total_page = findTotalPage(first_url)
 
-page = 0
-n_class = 0
-while (page < total_page ):
-    page += 1
-    url = 'https://hahow.in/courses?search=python&page='+str(page)
-    mydriver0 = webdriver.Chrome(chromedriver_path , options=chrome_options)
-    gettingurl0 = mydriver0.get(url)  #
-    print("資料爬起來! >>> 第"+str(page)+"/"+str(total_page)+"頁：")
-    time.sleep(3)
-    source = mydriver0.page_source
-    soup = BeautifulSoup(source, 'html.parser')
-    class_block = soup.find_all('div',{'class':'sc-10r5mg2-0 fVNHJD hh-course-brief relative block '})  # 在ＧＤ上跑，最後面需要空格（我不知道為什麼）
-    for class_ in class_block :
-        n_class+=1
-        class_href = class_.find('div','cover-wrap relative').find('a')
-        class_url ='https://hahow.in'+ class_href.get('href')
-        mydriver1 = webdriver.Chrome(chromedriver_path , options=chrome_options)
-        gettingurl1 = mydriver1.get(class_url)
-        time.sleep(3)
-        response_source = mydriver1.page_source
-        response_soup = BeautifulSoup(response_source, 'html.parser')  
-        title = response_soup.find('h1').text
-        if 'Python' in title : # 有符合條件才找
-            img = response_soup.find('div',class_='plyr__video-wrapper').find('video')
-            # img_url
-            img_url = img.get('poster')
-            # price
-            if response_soup.find('h4',class_='marg-tb-0')  == None and response_soup.find('h1',class_='price') == None :
-                price = response_soup.find('h2').text
-            elif response_soup.find('div',class_='text-sm marg-tb-0') == None and response_soup.find('div',class_='proposal-pricing') == None :
-                price = response_soup.find('h1',class_='price').text
-            else:
-                price = response_soup.find('h4',class_='marg-tb-0').text      
-            # teacher_name  
-            teacher_data = response_soup.find_all('div',class_='sc-1l1teqs-0 iFCjAI')
-            for teacher in teacher_data:
-                teacher_list = teacher.text
-                if '老師' in teacher_list :
-                    teacher_name = teacher_list[5:]
+def findPrice(my_soup):
+    """ Drinks the soup and finds the price of the course"""
+    the_price = 'None'
+    if my_soup.find('h4',class_='marg-tb-0')  == None and my_soup.find('h1',class_='price') == None :
+        the_price = my_soup.find('h2').text
+    elif my_soup.find('div',class_='text-sm marg-tb-0') == None and my_soup.find('div',class_='proposal-pricing') == None :
+        the_price = my_soup.find('h1',class_='price').text
+    else:
+        the_price = my_soup.find('h4',class_='marg-tb-0').text 
+    return the_price
 
-            url_feedback = class_url +'/feedbacks'
-            mydriver2 = webdriver.Chrome('chromedriver', options=chrome_options)
-            gettingurl2 = mydriver2.get(url_feedback)
+def findTeacher(my_soup):
+    """ Drinks the soup and finds out who teaches the course"""
+    teacher_data = my_soup.find_all('div',class_='sc-1l1teqs-0 iFCjAI')
+    the_teacher_name = 'None'
+    for teacher in teacher_data:
+        teacher_list = teacher.text
+        if '老師' in teacher_list :
+            the_teacher_name = teacher_list[5:]
+    return the_teacher_name
+
+def rollOutThePage(my_driver):
+    """ Presses "看更多" button until we see the end of the page
+        then returns the number of times that the button is pressed 
+    :param my_driver: selenium driver that is loading the page
+    """
+    haveButton = True
+    n_press = 0
+    while haveButton == True:
+        try: # when there is the continue button
+            actions = ActionChains(my_driver)
+            seeMoreButton = my_driver.find_elements_by_xpath("//button[@class='sc-1a6j6ze-0 cYdxxq b21euj-2 gMMXlv']")[0] 
+            actions.click(seeMoreButton)
+            n_press+=1
+            actions.perform()
             time.sleep(3)
+        except:
+            haveButton = False
+    print('pressed '+str(n_press)+' times')
 
-            haveButton = True
-            n_press = 0
-            while haveButton == True:
-                try: # when there is the continue button
-                    actions = ActionChains(mydriver2)
-                    seeMoreButton = mydriver2.find_elements_by_xpath("//button[@class='sc-1a6j6ze-0 cYdxxq b21euj-2 gMMXlv']")[0] # 看更多
-                    actions.click(seeMoreButton)
-                    n_press+=1
-                    actions.perform()
-                    time.sleep(3)
-                except:
-                    haveButton = False 
-            # After pressing button 'n_press' times ...
-            source2 =  mydriver2.page_source
-            soup2 = BeautifulSoup(source2, 'html.parser')            
-            comments = soup2.find_all('div',{'class':'wei2cc-1 gUylJK marg-b-25'})
-            count = 0 # reinitialize nc
-            stars = []; dates = []; shortTitles = []; longComments = []
-            for comment in comments:
-                count+=1
-                starRating = comment.find('p',{'class':'marg-b-0'})
-                rating = comment.find('div',{'class':'star-ratings'})
-                star = rating.attrs['title']
-                date = comment.find('time').text
-                shortTitle = comment.find('p',{'class':'text-strong marg-b-5'}).text
-                longComment = comment.find('p',class_='marg-b-0').text
-                stars.append(star)
-                dates.append(date)
-                shortTitles.append(shortTitle)
-                longComments.append(longComment)
-            if count!=0: # 如果有搜到評論 
-                df_of_1_course = pd.DataFrame({\
-                                   "class_title"   : title, \
-                                   "class_url"     : class_url, \
-                                   "class_figure"  : img_url, \
-                                   "stars"         : stars, \
-                                   "teacher_name"  : teacher_name, \
-                                   "price"         : price, \
-                                   "comment_date"  : dates, \
-                                   "comment_title" : shortTitles, \
-                                   "comment_text"  : longComments}) 
-                # update the dataframe
-                df = df.append(df_of_1_course, ignore_index = True) 
-            print('---第'+str(n_class)+'筆------按了'+str(n_press)+'次------收錄'+ str(count)+ '個評論-----------') 
-            mydriver2.quit() 
+def crawlFeedbackPage(my_url):
+    """ Crawls the feedback page and return number of comments found and their info """
+    url_feedback = my_url +'/feedbacks'
+    driver = webdriver.Chrome('chromedriver', options=chrome_options)
+    gettingurl = driver.get(url_feedback)
+    time.sleep(3)
+
+    rollOutThePage(driver)
+
+    soup = BeautifulSoup(driver.page_source, 'html.parser')            
+    comments = soup.find_all('div',{'class':'wei2cc-1 gUylJK marg-b-25'})
+    count = 0 
+    stars = []; dates = []; shortTitles = []; longComments = []
+    for comment in comments:
+        count+=1
+        starRating = comment.find('p',{'class':'marg-b-0'})
+        rating = comment.find('div',{'class':'star-ratings'})
+        star = rating.attrs['title']
+        date = comment.find('time').text
+        shortTitle = comment.find('p',{'class':'text-strong marg-b-5'}).text
+        longComment = comment.find('p',class_='marg-b-0').text
+        stars.append(star)
+        dates.append(date)
+        shortTitles.append(shortTitle)
+        longComments.append(longComment)
+    driver.quit()
+    return count, stars, dates, shortTitles, longComments 
+
+def updateDataFrame(my_df, title,class_url,img_url,stars,teacher_name,price,dates,shortTitles,longComments):
+    df_of_1_course = pd.DataFrame({\
+                   "class_title"   : title, \
+                   "class_url"     : class_url, \
+                   "class_figure"  : img_url, \
+                   "stars"         : stars, \
+                   "teacher_name"  : teacher_name, \
+                   "price"         : price, \
+                   "comment_date"  : dates, \
+                   "comment_title" : shortTitles, \
+                   "comment_text"  : longComments}) 
+    # add to the existing dataframe
+    my_df = my_df.append(df_of_1_course, ignore_index = True) 
+    return my_df
+
+def crawlCoursePage(my_df,course):
+    class_href = course.find('div','cover-wrap relative').find('a')
+    class_url ='https://hahow.in'+ class_href.get('href')
+    driver = webdriver.Chrome(chromedriver_path , options=chrome_options)
+    gettingurl = driver.get(class_url)
+    time.sleep(3)
+    soup = BeautifulSoup(driver.page_source, 'html.parser')  
+    title = soup.find('h1').text
+
+    if 'Python' in title: 
+
+        img = soup.find('div',class_='plyr__video-wrapper').find('video')
+        img_url = img.get('poster')
+
+        price = findPrice(soup)
+
+        teacher_name = findTeacher(soup)
+
+        count, stars, dates, shortTitles, longComments = crawlFeedbackPage(class_url)
+
+        if count!=0: # if there is any comments found
+            my_df = updateDataFrame(my_df, title,class_url,img_url,stars,teacher_name,price,dates,shortTitles,longComments)
+            print('---------收錄'+ str(count)+ '個評論-----------') 
         else:
-            print('---第'+str(n_class)+'筆-----課程名稱沒有'+target)
-        mydriver1.quit() 
-    mydriver0.quit()
+            print('---------此課程還沒有評論-----------')
+    else:
+        print('---------沒有需要爬此課程-----------')
+    driver.quit()
+    return my_df
 
 
-# save file 
-current_date = datetime.datetime.now().strftime ('%d%b%Y-%H%M%S')
-current_folder = Path('.')
-csv_name = str(page)+'page_result_'+str(current_date)+'.csv'
-path_to_file = current_folder / csv_name 
-df.to_csv(path_to_file)
+def allCommentResearch(my_target):
+    # initialise dataframe
+    df = pd.DataFrame({\
+    "class_title"   : [], \
+    "class_url"     : [], \
+    "class_figure"  : [], \
+    "stars"         : [], \
+    "teacher_name"  : [], \
+    "price"         : [], \
+    "comment_date"  : [], \
+    "comment_title" : [], \
+    "comment_text"  : []}) 
+
+    search_page_url = 'https://hahow.in/courses?search='+my_target
+
+    total_page = findTotalPage(search_page_url)
+    page = 0
+    n_class = 0
+    while (page < total_page):
+        page += 1
+        url = 'https://hahow.in/courses?search=python&page='+str(page)
+        driver = webdriver.Chrome(chromedriver_path , options=chrome_options)
+        gettingurl = driver.get(url)  
+        print("資料爬起來! >>> 第"+str(page)+"/"+str(total_page)+"頁：")
+        time.sleep(3)
+        soup = BeautifulSoup(driver.page_source, 'html.parser')
+        class_block = soup.find_all('div',{'class':'sc-10r5mg2-0 fVNHJD hh-course-brief relative block'}) # might need (or not) a blank space at the end of 'class' 
+        for class_ in class_block :
+            df = crawlCoursePage(df,class_)
+            n_class+=1
+            
+        driver.quit()
+    return df
+
+def saveDataframe(df):
+    """ Saves the dataframe into csv format with a timestamp in the current folder """
+    current_date = datetime.datetime.now().strftime ('%d%b%Y-%H%M%S')
+    current_folder = Path('.')
+    csv_name = 'Comments_'+str(current_date)+'.csv'
+    path_to_file = current_folder / csv_name 
+    df.to_csv(path_to_file)
+    print("file saved: " + str(path_to_file))
 
 
+def main():
+    target = 'python'
+    my_dataframe = allCommentResearch(target)
+    saveDataframe(my_dataframe)
+
+if __name__ == "__main__":
+    main()
